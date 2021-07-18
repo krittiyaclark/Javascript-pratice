e030fc5e616ad95886447ac4ef7e5778297f941b
Krittiya <krittiyaclark@gmail.com>
2021-07-18 19:50:01 -0400

    Adds Javascript practice project class30

---
+++ class30/node-intro-demo/dist/demo.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var http = require('http');
+
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  fs.readFile('demofile.html', function (err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.write(data);
+    res.end();
+  });
+}).listen(8000); // const http = require('http')
+// const fs = require('fs')
+// http.createServer((req, res) => {
+//   fs.readFile('demofile.html', (err, data) => {
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.write(data)
+//     res.end()
+//   })
+// }).listen(8000)
