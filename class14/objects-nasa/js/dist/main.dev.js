7455b9edc8370dc83f8efb47dab63788a3e1794e
Krittiya <krittiyaclark@gmail.com>
2021-07-25 15:50:04 -0400

    Adds Javascript practice project class14

---
+++ class14/objects-nasa/js/dist/main.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector('button').addEventListener('click', function () {
+  var date = document.querySelector('input').value;
+  fetch("https://api.nasa.gov/planetary/apod?api_key=eMoK5KtSfosiWbeohle3sgKGx2ZqO0TPx68GftCf&date=".concat(date)).then(function (res) {
+    return res.json();
+  }) // parse response as JSON
+  .then(function (data) {
+    console.log(data);
+
+    if (data.media_type === 'image') {
+      document.querySelector('h2').innerText = data.title;
+      document.querySelector('img').src = data.hdurl;
+      document.querySelector('h3').innerText = data.explanation;
+    } else if (data.media_type === 'video') {
+      document.querySelector('h2').innerText = data.title;
+      document.querySelector('iframe').src = data.url;
+      document.querySelector('h3').innerText = data.explanation;
+    }
+  })["catch"](function (err) {
+    console.log("error ".concat(err));
+  });
+});
